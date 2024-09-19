e74b271fa14a037ace509aa0389f4a2032cd0b1f
gertrude654 <gertrude.uzimana@amalitechtraining.org>
2024-09-19 12:04:20 +0200

    ft:initialize jenkins lab

---
+++ src/main/java/com/jenkins/jenkinslab/JenkinslabApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinslabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinslabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/jenkinslab/JenkinslabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinslabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
